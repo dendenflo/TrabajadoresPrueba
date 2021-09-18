9c3865b5dabfac1e3a5b896e39a815a7cde75954
Denis Flores <u20191A772@upc.edu.pe>
2021-09-18 17:31:34 -0500

    Denis-Entidades

---
+++ TrabajadoresPrueba/TrabajadoresPrueba/Entidades/Distrito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabajadoresPrueba.Entidades
+{
+    public class Distrito
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdProvincia { get; set; }
+        public string NombreDistrito { get; set; }
+    }
+}
---
+++ TrabajadoresPrueba/TrabajadoresPrueba/Entidades/Trabajadores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabajadoresPrueba.Entidades
+{
+    public class Trabajadores
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string TipoDocumento { get; set; }
+        public string Nombres { get; set; }
+        public string Sexo { get; set; }
+        public int IdDepartamento { get; set; }
+        public int IdProvincia { get; set; }
+        public int IdDistrito { get; set; }
+    }
+}
